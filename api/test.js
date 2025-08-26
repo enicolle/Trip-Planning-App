c9b8e0874969969c02efded9564bb5f652e98819
enicolle <enicolle12@gmail.com>
2025-08-26 11:28:33 -0500

    Delete api/test.js

--- api/test.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  return res.status(200).json({
-    message: 'Hello from Vercel!',
-    method: req.method,
-    url: req.url,
-    timestamp: new Date().toISOString()
-  });
-}
